2d09ab5af74015c332829bba86029d7cb1190eae
Tyzer1 <Tizara1@protonmail.com>
2022-06-03 19:05:02 +0300

    Toggle button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactDOM2 from 'react-dom';
 import './index.css';
 //import App from './App';
 //import reportWebVitals from './reportWebVitals';
@@ -44,10 +45,35 @@ class Clock extends React.Component {
   }
 }
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // Эта привязка обязательна для работы `this` в колбэке.
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'Включено' : 'Выключено'}
+      </button>
+    );
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Clock />
+  <Toggle />
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
